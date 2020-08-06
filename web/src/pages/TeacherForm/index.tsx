79cd60cb0659cd2181de80ce6847814e8f4c6800
Jessica Marques <jessica.marques.dev@gmail.com>
2020-08-05 22:51:15 -0300

    commit web

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,10 +1,110 @@
-import React from 'react';
-import PageHeader from '../../components/PageHeader';
+import React, {useState} from 'react';
 
-function TeacherForm(){
+import PageHeader from '../../components/PageHeader';
+import Textarea from '../../components/Textarea';
+import Select from '../../components/Select';
+import Input from '../../components/Input';
+
+import './styles.css';
+
+import warningIcon from '../../assets/images/icons/warning.svg'
+
+function TeacherForm() {
+    const [scheduleItems, setScheduleItems] = useState([
+        { week_day: 0, from: '', to: ''}
+    ]);
+
+    function addNewScheduleItem(){
+        setScheduleItems([
+            ...scheduleItems,
+            { week_day: 0, from: '', to: ''}
+        ]);
+    }
     return (
         <div id="page-teacher-form" className="container">
-           <PageHeader title ="Que incrível que você quer dar aulas."/>
+            <PageHeader
+                title="Que incrível que você quer dar aulas."
+                description="O primeiro passo é preencher esse formulário de inscrição"
+            />
+            <main>
+                <fieldset>
+                    <legend>Seus dados</legend>
+
+                    <Input name="name" label="Nome Completo" />
+                    <Input name="avatar" label="Avatar" />
+                    <Input name="whatsapp" label="Whatsapp" />
+                    <Textarea name="bio" label="Biografia" />
+
+                </fieldset>
+                <fieldset>
+                    <legend>Sobre a aula</legend>
+
+                    <Select
+                        name="subject"
+                        label="Matéria"
+                        options={[
+                            { value: 'Artes', label: 'Artes' },
+                            { value: 'Português', label: 'Português' },
+                            { value: 'Redação', label: 'Redação' },
+                            { value: 'Matemática', label: 'Matemática' },
+                            { value: 'Física', label: 'Física' },
+                            { value: 'História', label: 'História' },
+                            { value: 'Geografia', label: 'Geografia' },
+                            { value: 'Sociologia', label: 'Sociologia' },
+                            { value: 'Filosofia', label: 'Filosofia' },
+                            { value: 'Biologia', label: 'Biologia' },
+                            { value: 'Química', label: 'Química' },
+                            { value: 'Inglês', label: 'Inglês' },
+                            { value: 'Espanhol', label: 'Espanhol' },
+                            { value: 'Educação Física', label: 'Educação Física' },
+                        ]}
+                    />
+                    <Input name="cost" label="Custo da sua hora por aula " />
+
+                </fieldset>
+                <fieldset>
+                    <legend>
+                        Horários Disponíveis
+                   <button type="button" onClick = {addNewScheduleItem}>
+                            + Novo horário
+                   </button>
+                    </legend>
+
+                    {scheduleItems.map(scheduleItem => {
+                        return (
+                            <div key={scheduleItem.week_day} className="schedule-item">
+                                <Select
+                                    name="week_day"
+                                    label="Dia da Semana"
+                                    options={[
+                                        { value: '0', label: 'Domingo' },
+                                        { value: '1', label: 'Segunda-feira' },
+                                        { value: '2', label: 'Terça-feira' },
+                                        { value: '3', label: 'Quarta-feira' },
+                                        { value: '4', label: 'Quinta-feira' },
+                                        { value: '5', label: 'Sexta-feira' },
+                                        { value: '6', label: 'Sábado' },
+                                        { value: '7', label: 'Domingo' },
+                                    ]}
+                                />
+                                <Input name="from" label="Das" type="time" />
+                                <Input name="to" label="Até" type="time" />
+                            </div>
+                        );
+                    })}
+                </fieldset>
+
+                <footer>
+                    <p>
+                        <img src={warningIcon} alt="Aviso importante" />
+                       Importante <br />
+                       Preencha todos os dados
+                   </p>
+                    <button type='button'>
+                        Salvar Cadastro
+                   </button>
+                </footer>
+            </main>
         </div>
     )
 }
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -3,24 +3,49 @@ import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem'
 
 import './styles.css';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
 
 function TeacherList(){
     return (
         <div id="page-teacher-list" className="container">
             <PageHeader title="Estes são os proffys disponíveis.">
                 <form id="search-teachers">
-                    <div className="input-block">
-                        <label htmlFor="subject">Matéria</label>
-                        <input type="text" id="subject"/>    
-                    </div>   
-                    <div className="input-block">
-                        <label htmlFor="week_day">Dia da semana</label>
-                        <input type="text" id="week_day"/>    
-                    </div> 
-                    <div className="input-block">
-                        <label htmlFor="time">Hora</label>
-                        <input type="text" id="time"/>    
-                    </div>  
+                <Select 
+                    name = "subject" 
+                    label = "Matéria" 
+                    options={[
+                        {value: 'Artes', label: 'Artes'},
+                        {value: 'Português', label: 'Português'},
+                        {value: 'Redação', label: 'Redação'},
+                        {value: 'Matemática', label: 'Matemática'},
+                        {value: 'Física', label: 'Física'},
+                        {value: 'História', label: 'História'},
+                        {value: 'Geografia', label: 'Geografia'},
+                        {value: 'Sociologia', label: 'Sociologia'},
+                        {value: 'Filosofia', label: 'Filosofia'},
+                        {value: 'Biologia', label: 'Biologia'},
+                        {value: 'Química', label: 'Química'},
+                        {value: 'Inglês', label: 'Inglês'},
+                        {value: 'Espanhol', label: 'Espanhol'},
+                        {value: 'Educação Física', label: 'Educação Física'},
+                    ]}
+                   />
+                   <Select 
+                    name = "week_day" 
+                    label = "Dia da Semana"
+                    options={[
+                        {value: '0', label: 'Domingo'},
+                        {value: '1', label: 'Segunda-feira'},
+                        {value: '2', label: 'Terça-feira'},
+                        {value: '3', label: 'Quarta-feira'},
+                        {value: '4', label: 'Quinta-feira'},
+                        {value: '5', label: 'Sexta-feira'},
+                        {value: '6', label: 'Sábado'},
+                        {value: '7', label: 'Domingo'},
+                    ]}
+                   />
+                    <Input type= "time" name = "time" label = "Hora" />
                 </form> 
             </PageHeader> 
             <main>
